bc4bf9ea21fa5bc0c106ecaeeab802c537f220df
Anan Jaser <ananjaser@gmail.com>
2021-11-15 18:23:21 +0400

    check that the dataset exists before doing anything

--- SA.py
+++ SA.py
@@ -6,6 +6,11 @@ import os
 
 # Data set File name / path
 dataset_file='amazon_reviews_us_Mobile_Electronics_v1_00.tsv'
+if os.path.exists(dataset_file):
+    print("Using " + os.path.splitext(dataset_file)[0] + " Dataset file...")
+else:
+    print("Dataset file is missing")
+    exit()
 
 # Convert amazon dataset to CSV container
 # Delete previously generated dataset files
@@ -13,7 +18,7 @@ old_dataset = 'dataset.csv'
 if os.path.exists(old_dataset):
     print("Clean up old datasets...")
     os.remove(old_dataset)
-print("Preparing Dataset file " + os.path.splitext(dataset_file)[0] + "...")
+print("Preparing Dataset file ...")
 print("Converting Dataset to supported CSV format... \nIgnoring broken entries...")
 csv_table=pd.read_table(dataset_file,sep='\t',error_bad_lines=False,warn_bad_lines=False)
 csv_table.to_csv('dataset.csv',index=False)
